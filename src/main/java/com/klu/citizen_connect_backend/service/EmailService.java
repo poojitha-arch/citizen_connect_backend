494ab3725b826dc34361ea613157561f063bf655
poojitha-arch <poojitha27.ai@gmail.com>
2026-04-28 10:13:55 +0530

    prevent mail failure from blocking signup

--- src/main/java/com/klu/citizen_connect_backend/service/EmailService.java
+++ src/main/java/com/klu/citizen_connect_backend/service/EmailService.java
@@ -1,7 +1,5 @@
 package com.klu.citizen_connect_backend.service;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,29 +11,46 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendOtpEmail(String toEmail, String username, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Citizen Connect - OTP Verification");
-        message.setText(
-                "Hello " + username + ",\n\n" +
-                "Your OTP for Citizen Connect registration is: " + otp + "\n\n" +
-                "This OTP is valid for 5 minutes.\n\n" +
-                "Regards,\nCitizen Connect Team"
-        );
-        mailSender.send(message);
+    public boolean sendOtpEmail(String toEmail, String username, String otp) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(toEmail);
+            message.setSubject("Citizen Connect - OTP Verification");
+            message.setText(
+                    "Hello " + username + ",\n\n" +
+                    "Your OTP for Citizen Connect registration is: " + otp + "\n\n" +
+                    "This OTP is valid for 5 minutes.\n\n" +
+                    "Regards,\nCitizen Connect Team"
+            );
+
+            mailSender.send(message);
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Mail sending failed: " + e.getMessage());
+            System.out.println("OTP for " + toEmail + " is: " + otp);
+            return false;
+        }
     }
 
-    public void sendLoginEmail(String toEmail, String username) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Citizen Connect - Login Alert");
-        message.setText(
-                "Hello " + username + ",\n\n" +
-                "Your account was logged in successfully.\n\n" +
-                "If this was not you, please reset your password immediately.\n\n" +
-                "Regards,\nCitizen Connect Team"
-        );
-        mailSender.send(message);
+    public boolean sendLoginEmail(String toEmail, String username) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(toEmail);
+            message.setSubject("Citizen Connect - Login Alert");
+            message.setText(
+                    "Hello " + username + ",\n\n" +
+                    "Your account was logged in successfully.\n\n" +
+                    "If this was not you, please reset your password immediately.\n\n" +
+                    "Regards,\nCitizen Connect Team"
+            );
+
+            mailSender.send(message);
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Login mail failed: " + e.getMessage());
+            return false;
+        }
     }
 }
